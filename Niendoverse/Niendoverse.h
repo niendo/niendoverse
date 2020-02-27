447bfeb921fd73b75c9eeeada5cc21a622e7695a
Eduardo Nieto <enieto@vectoritcgroup.com>
2020-02-27 19:24:58 +0100

    Before time and space began there was ..... loneliness

---
+++ Niendoverse/Niendoverse.h
@@ -0,0 +1,19 @@
+//
+//  Niendoverse.h
+//  Niendoverse
+//
+//  Created by Niendo on 27/02/2020.
+//  Copyright © 2020 Niendo's. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Niendoverse.
+FOUNDATION_EXPORT double NiendoverseVersionNumber;
+
+//! Project version string for Niendoverse.
+FOUNDATION_EXPORT const unsigned char NiendoverseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Niendoverse/PublicHeader.h>
+
+
